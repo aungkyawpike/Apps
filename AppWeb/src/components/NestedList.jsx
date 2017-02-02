bde491ddc9ea94952738fb83ee0c749a9729e42a
aungkyawpike.akp <aungkyawpike.akp@gmail.com>
2017-02-03 00:58:39 +0800

    add selectable list

--- AppWeb/src/components/NestedList.jsx
+++ AppWeb/src/components/NestedList.jsx
@@ -1,21 +1,35 @@
-import React from 'react'
+import React, {Component, PropTypes} from 'react'
 import ReactDOM from 'react-dom'
 import {Card, CardActions, CardHeader, CardText} from 'material-ui/Card'
-import {List, ListItem} from 'material-ui/List'
+import {List, ListItem, makeSelectable} from 'material-ui/List'
+import ListExampleSelectable from '../components/ListExampleSelectable'
 
 export default class NestedList extends React.Component {
 
 	constructor(props) {
 		super(props)
+		this.state = {selectedIndex: 1}
 	}
 
-	recursiveProcess = (dataList,subDataListName, listItems)=> {
+	componentWillMount() {
+		this.setState({
+			selectedIndex: 1
+		})
+	}
+
+	handleRequestChange = (event, index) => {
+		this.setState({
+			selectedIndex: index
+		})
+	}
+
+	recursiveProcess = (dataList, subDataListName, listItems)=> {
 		for (var dataItem of dataList) {
 			let nestedListItems = []
 			if (dataItem[subDataListName].length > 0) {
-				this.recursiveProcess(dataItem[subDataListName],subDataListName, nestedListItems)
+				this.recursiveProcess(dataItem[subDataListName], subDataListName, nestedListItems)
 			}
-			listItems.push(<ListItem style={{fontSize:13}} key={dataItem.id} primaryText={dataItem.name}
+			listItems.push(<ListItem value={dataItem.id} key={dataItem.id} style={{fontSize:13}} primaryText={dataItem.name}
 															 primaryTogglesNestedList={true}
 															 nestedItems={nestedListItems}/>)
 		}
@@ -36,11 +50,10 @@ export default class NestedList extends React.Component {
 					titleStyle={{fontSize:13}}
 				/>
 				<CardActions expandable={true}>
-					<List>
-						{listItems}
-					</List>
+								<ListExampleSelectable/>
 				</CardActions>
 			</Card>
+
 		)
 	}
 
