112c93a8c7948323f7bab0617af68492bc791ef1
aungkyawpike <aungkyawpike.akp@gmail.com>
2017-04-20 19:17:52 +0800

    fix testing

--- AppWeb/src/test/routes/products.js
+++ AppWeb/src/test/routes/products.js
@@ -1,33 +1,34 @@
-let sw = require('selenium-webdriver');
-let chai = require('chai');
-let should = chai.should();
-let chaiWebdriver = require('chai-webdriver');
+let webdriver = require('selenium-webdriver');
 let chrome = require('selenium-webdriver/chrome');
 let path = require('chromedriver').path;
+let chai = require('chai');
+let should = chai.should();
 
-let service = new chrome.ServiceBuilder(path).build();
-chrome.setDefaultService(service);
-let driver = new sw.Builder()
-	.withCapabilities(sw.Capabilities.chrome())
+//let service = new chrome.ServiceBuilder(path).build();
+//chrome.setDefaultService(service);
+//let driver = new sw.Builder()
+//	.withCapabilities(sw.Capabilities.chrome())
+//	.build()
+const driver = new webdriver.Builder()
+	.forBrowser('chrome')
 	.build()
-chai.use(chaiWebdriver(driver));
+const By = webdriver.By;
 
 describe('products', function() {
   this.timeout(100000);
-	before(function(done) {
-		//driver.get('http://localhost:8888').then(() => done())
-		driver.get('http://github.com').then(() => done());
+	before(async function(done) {
+		driver.navigate().to('http://localhost:8888').then(() => done());
 	});
 
 	describe('route to /products', () => {
 		it('it should show all the products', (done) => {
-			//chai.expect('#site-container h1.heading').dom.to.not.contain.text("I'm a kitty!");
-			chai.expect('#js-pjax-container').dom.to.not.contain.text("I'm a kitty!");
+				driver.findElement(By.css('#container2'))
+					.then((container) => chai.expect(container).not.to.be.null)
+					.then(() => done());
 		});
 	});
 
 	after((done) => {
-		driver.quit()
-			.then(() => done())
+		driver.quit().then(() => done());
 	});
 })
