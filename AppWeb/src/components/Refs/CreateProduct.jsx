85281996dfaa668769002845fe65c10bd752160f
aungkyawpike <aungkyawpike.akp@gmail.com>
2017-05-04 18:57:55 +0800

    fix form

--- AppWeb/src/components/Refs/CreateProduct.jsx
+++ AppWeb/src/components/Refs/CreateProduct.jsx
@@ -17,6 +17,8 @@ class CreateProduct extends React.Component {
 		this.state = {
 			condition: 'new',
 			title: '',
+			titleError: '',
+			titleBorderRedCss: '',
 			description: '',
 			price: '',
 			name: '',
@@ -42,15 +44,7 @@ class CreateProduct extends React.Component {
 				}
 			}
 		}
-	}
 
-	componentDidMount(){
-		// To disabled submit button at the beginning.
-		this.props.form.validateFields();
-	}
-
-	 hasErrors = (fieldsError) => {
-		 return Object.keys(fieldsError).some(field => fieldsError[field]);
 	}
 
 	handleConditionChange = (value) => {
@@ -60,9 +54,21 @@ class CreateProduct extends React.Component {
 	}
 
 	handleTitleChange = (event) =>{
-		this.setState({
-			title : event.target.value
-		});
+		var value = event.target.value.trim()
+		var errors = validate.single(value, {presence:true, length:{minimum:1}})
+		if(!errors){
+			this.setState({
+				title : value,
+				titleError : '',
+				titleBorderRedCss : ''
+			});
+		}
+		else{
+			this.setState({
+				titleError : errors.join(),
+				titleBorderRedCss : 'borderRed'
+			});
+		}
 	}
 
 	handleDescriptionChange = (event) => {
@@ -71,9 +77,9 @@ class CreateProduct extends React.Component {
 		});
 	}
 
-	handlePriceChange = (value) => {
+	handlePriceChange = (event) => {
 		this.setState({
-			price : value
+			price : event.target.value
 		});
 	}
 
@@ -101,41 +107,26 @@ class CreateProduct extends React.Component {
 		});
 	}
 
-	handleSubmit = (e) => {
-		e.preventDefault();
-		this.props.form.validateFields((err, values) => {
-			if (!err) {
-				console.log('Received values of form: ', values);
-			}
-		});
+	handleSubmit = () => {
+
 	}
 
 	render() {
-		const { getFieldDecorator, getFieldsError, getFieldError, isFieldTouched } = this.props.form;
-		const conditionError = isFieldTouched('condition') && getFieldError('condition');
-		const passwordError = isFieldTouched('password') && getFieldError('password');
-		const titleError = isFieldTouched('title') && getFieldError('title');
-		const descriptionError = isFieldTouched('description') && getFieldError('description');
-		const priceError = isFieldTouched('price') && getFieldError('price');
-		const nameError = isFieldTouched('name') && getFieldError('name');
-		const emailError = isFieldTouched('email') && getFieldError('email');
-		const phoneError = isFieldTouched('phone') && getFieldError('phone');
-		const addressError = isFieldTouched('address') && getFieldError('address');
+		const { getFieldDecorator } = this.props.form;
 
 		return (
-				<Form onSubmit={this.handleSubmit} action="//jsonplaceholder.typicode.com/posts/" method="post">
+				<Form onSubmit={this.handleSubmit}>
 					<div>
 						<Row>
 							<Col xs={6} sm={6} md={2} lg={2}>
 								<div>Condition</div>
 							</Col>
 							<Col xs={12} sm={12} md={12} lg={12}>
-								<FormItem validateStatus={conditionError ? 'error' : ''} help={conditionError || ''}>
+								<FormItem>
 									{getFieldDecorator('condition', {
 										rules: [
-											{ required: true, message: 'Please select Condition' }
+											{ required: true, message: 'Please select Condition!' },
 										],
-										initialValue: 'new'
 									})(
 										<Select defaultValue="new" style={{ width: '100%' }} onChange={this.handleConditionChange}>
 											<Option value="new">New</Option>
@@ -150,10 +141,10 @@ class CreateProduct extends React.Component {
 								<div>Title</div>
 							</Col>
 							<Col xs={12} sm={12} md={12} lg={12}>
-								<FormItem validateStatus={titleError ? 'error' : ''} help={titleError || ''}>
+								<FormItem>
 									{getFieldDecorator('title', {
 										rules: [
-											{ required: true, message: 'Please add title' },
+											{ required: true, message: 'Please input title!' },
 										],
 									})(
 										<Input className={this.state.titleBorderRedCss} onChange={this.handleTitleChange}/>
@@ -162,19 +153,17 @@ class CreateProduct extends React.Component {
 							</Col>
 						</Row>
 						<Row>
+							<Col xs={6} sm={6} md={2} lg={2}></Col>
+							<Col xs={12} sm={12} md={12} lg={12}>
+								<div className="red">{this.state.titleError}</div>
+							</Col>
+						</Row>
+						<Row>
 							<Col xs={6} sm={6} md={2} lg={2}>
 								<div>Description</div>
 							</Col>
 							<Col xs={12} sm={12} md={12} lg={12}>
-								<FormItem validateStatus={descriptionError ? 'error' : ''} help={descriptionError || ''}>
-									{getFieldDecorator('description', {
-										rules: [
-											{ required: true, message: 'Please add description' },
-										],
-									})(
-											<Input type="textarea" onChange={this.handleDescriptionChange}/>
-									)}
-								</FormItem>
+								<Input type="textarea" onChange={this.handleDescriptionChange}/>
 							</Col>
 						</Row>
 						<Row>
@@ -183,21 +172,13 @@ class CreateProduct extends React.Component {
 							</Col>
 							<Col xs={12} sm={12} md={12} lg={12}>
 								<div style={{ marginTop: 16, height: 180 }}>
-									<FormItem>
-										{getFieldDecorator('photos', {
-											rules: [
-												{ required: true, message: 'Please add photos' },
-											],
-										})(
-											<Dragger {...this.uploadProps}>
-												<p className="ant-upload-drag-icon">
-													<Icon type="inbox" />
-												</p>
-												<p className="ant-upload-text">Click or drag file to this area to upload</p>
-												<p className="ant-upload-hint">Support for a single or bulk upload. Strictly prohibit from uploading company data or other band files</p>
-											</Dragger>
-										)}
-									</FormItem>
+									<Dragger {...this.uploadProps}>
+										<p className="ant-upload-drag-icon">
+											<Icon type="inbox" />
+										</p>
+										<p className="ant-upload-text">Click or drag file to this area to upload</p>
+										<p className="ant-upload-hint">Support for a single or bulk upload. Strictly prohibit from uploading company data or other band files</p>
+									</Dragger>
 								</div>
 							</Col>
 						</Row>
@@ -205,7 +186,7 @@ class CreateProduct extends React.Component {
 							<Col xs={6} sm={6} md={2} lg={2}>
 								<div>Price</div>
 							</Col>
-							<Col xs={4} sm={4} md={6} lg={6}>
+							<Col xs={12} sm={12} md={6} lg={6}>
 								<Select defaultValue="amount" style={{ width: '100%' }} onChange={this.handlePriceChange}>
 									<Option value="amount">Amount</Option>
 									<Option value="free">Free</Option>
@@ -213,16 +194,8 @@ class CreateProduct extends React.Component {
 									<Option value="swap">Swap/Trade</Option>
 								</Select>
 							</Col>
-							<Col xs={8} sm={8} md={8} lg={6}>
-								<FormItem validateStatus={priceError ? 'error' : ''} help={priceError || ''}>
-									{getFieldDecorator('price', {
-										rules: [
-											{ required: true, message: 'Please add price' },
-										],
-									})(
-										<Input addonBefore="$"/>
-									)}
-								</FormItem>
+							<Col xs={12} sm={12} md={12} lg={6}>
+								<Input addonBefore="$"/>
 							</Col>
 						</Row>
 						<Row>
@@ -230,47 +203,23 @@ class CreateProduct extends React.Component {
 								<div>Name</div>
 							</Col>
 							<Col xs={12} sm={12} md={12} lg={12}>
-								<FormItem validateStatus={nameError ? 'error' : ''} help={nameError || ''}>
-									{getFieldDecorator('name', {
-										rules: [
-											{ required: true, message: 'Please add name' },
-										],
-									})(
 								<Input onChange={this.handleNameChange}/>
-									)}
-								</FormItem>
 							</Col>
 						</Row>
 						<Row>
 							<Col xs={6} sm={6} md={2} lg={2}>
-								<div>Email</div>
+								<div>Email Address</div>
 							</Col>
 							<Col xs={12} sm={12} md={12} lg={12}>
-								<FormItem validateStatus={emailError ? 'error' : ''} help={emailError || ''}>
-									{getFieldDecorator('email', {
-										rules: [
-											{ required: true, message: 'Please add email' },
-										],
-									})(
-										<Input onChange={this.handleEmailChange}/>
-									)}
-								</FormItem>
+								<Input onChange={this.handleEmailChange}/>
 							</Col>
 						</Row>
 						<Row>
 							<Col xs={6} sm={6} md={2} lg={2}>
-								<div>Phone</div>
+								<div>Phone Number</div>
 							</Col>
 							<Col xs={12} sm={12} md={12} lg={12}>
-								<FormItem validateStatus={phoneError ? 'error' : ''} help={phoneError || ''}>
-									{getFieldDecorator('phone', {
-										rules: [
-											{ required: true, message: 'Please add phone' },
-										],
-									})(
-										<Input onChange={this.handlePhoneChange}/>
-									)}
-								</FormItem>
+								<Input onChange={this.handlePhoneChange}/>
 							</Col>
 						</Row>
 						<Row>
@@ -278,21 +227,14 @@ class CreateProduct extends React.Component {
 								<div>Address</div>
 							</Col>
 							<Col xs={12} sm={12} md={12} lg={12}>
-								<FormItem validateStatus={addressError ? 'error' : ''} help={addressError || ''}>
-									{getFieldDecorator('address', {
-										rules: [
-											{ required: true, message: 'Please add email' },
-										],
-									})(
-										<Input onChange={this.handleAddressChange}/>
-									)}
-								</FormItem>
+								<Input onChange={this.handleAddressChange}/>
 							</Col>
 						</Row>
 						<Row>
 							<Col xs={6} sm={6} md={2} lg={2}></Col>
 							<Col xs={6} sm={6} md={12} lg={12}>
-								<Button htmlType="submit" disabled={this.hasErrors(getFieldsError())}>Post</Button>
+								<Button>Post</Button>
+								<Button>Preview</Button>
 							</Col>
 						</Row>
 					</div>
